f74dedee5e6036c985fbb2b60c93c1ca30e8dc4e
Mihir Ravat <mihirravat9901@gmail.com>
2026-04-16 15:39:31 +0530

    Created folders

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',   // ensures it runs on localhost
+    port: 5173           // default Vite port (you can change if needed)
+  }
 })
